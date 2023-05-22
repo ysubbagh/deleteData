af95c3cd585c4c91674b397e1fbb408a587074d5
ysubbagh <ydns10@gmail.com>
2023-05-21 21:15:02 -0700

    fix game details

--- fixPlays.py
+++ fixPlays.py
@@ -1,20 +1,22 @@
 import pandas as pd
+import numpy as np
 
 #csv file path
 GDPATH = "/Users/yasminesubbagh/Downloads/archive/games_details.csv"
 
 #read in the file
-dfgd = pd.read_csv(GDPATH)
+df = pd.read_csv(GDPATH, dtype={'Column6': str}, low_memory=False)
+df.fillna(np.nan, inplace=True)
 
 #ALTER the game details table
-dfgd.drop('TEAM_ABBREVIATION', inplace=True, axis=1)
-dfgd.drop('TEAM_CITY', inplace=True, axis=1)
-dfgd.drop('PLAYER_NAME', inplace=True, axis=1)
-dfgd.drop('NICKNAME', inplace=True, axis=1)
-dfgd.drop('PLAYER_NAME', inplace=True, axis=1)
 
-dfgd.columns = ['GameID', 'TeamID', 'PlayerID', 'StartPos', 'Comment', 'Minute', 'Field_Goals_Made', 'Field_Goals_Attemp', 'Field_Goal_Precent', '']
+df.drop('TEAM_ABBREVIATION', inplace=True, axis=1)
+df.drop('TEAM_CITY', inplace=True, axis=1)
+df.drop('PLAYER_NAME', inplace=True, axis=1)
+df.drop('NICKNAME', inplace=True, axis=1)
+
+df.columns = ['GameID', 'TeamID', 'PlayerID', 'StartPos', 'Comment', 'Minute', 'Field_Goals_Made', 'Field_Goals_Attempted', 'Field_Goal_Precent', '3Pt_Made', '3Pt_Attempted', '3Pt_Precent', 'Free_Throws_Made', 'Free_Throws_Attempted', 'Free_Throw_Precent', 'Offensive_Rebound', 'Defensive_Rebound', 'Total_Rebounds', 'Assists', 'Steals', 'Blocks', 'Turnovers', 'Personal_Fouls', 'Points', 'Plus_Minus']
 
 #output back into file, export
-dfgd.to_csv(GDPATH, index=False)
-print(dfgd)
+df.to_csv(GDPATH, index=False)
+print(df)
