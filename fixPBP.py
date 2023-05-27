cd4a3264c7fada9069673c1eed685e79bb525886
ysubbagh <ydns10@gmail.com>
2023-05-26 17:45:32 -0700

    fixed play by play

--- fixPBP.py
+++ fixPBP.py
@@ -8,7 +8,10 @@ PATH = '/Users/yasminesubbagh/Downloads/archive-3/csv/play_by_play.csv'
 df = pd.read_csv(PATH)
 
 #edit the file
-df['pcttimestring'] = df['pcttimestring'].fillna(0)
+df['pctimestring'] = df['pctimestring'].fillna(0)
+df.dropna(subset=['player1_id'], inplace=True)
+df = df.drop(['wctimestring', 'video_available_flag', 'player1_name', 'player1_team_city', 'player1_team_nickname', 'player1_team_abbreviation', 'player2_name', 'player2_team_city', 'player2_team_nickname', 'player2_team_abbreviation', 'player3_name', 'player3_team_city', 'player3_team_nickname', 'player3_team_abbreviation'], axis=1)
+df = df[df['player1_id'] != 1]
 
 #export / save the file
 df.to_csv(PATH, index=False)
